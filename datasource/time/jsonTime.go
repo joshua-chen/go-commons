4926338048d19144ca67663cb79e06676703729b
陈洲华 <joshua_chen@qq.com>
2021-01-04 15:06:08 +0800

    fx

--- datasource/time/jsonTime.go
+++ datasource/time/jsonTime.go
@@ -3,6 +3,11 @@ package time
 import "time"
 
 type JsonTime time.Time
+
 func (j JsonTime) MarshalJSON() ([]byte, error) {
-	return []byte(`"`+time.Time(j).Format("2006-01-02 15:04:05")+`"`), nil
+	t := time.Time(j)
+	if t.IsZero() {
+		return []byte(`""`), nil
+	}
+	return []byte(`"` + t.Format("2006-01-02 15:04:05") + `"`), nil
 }
